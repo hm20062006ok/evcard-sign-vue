3e1014e61029c03ae5b7ef3f6c1ee1f34d3e177c
HuMiao <humiao2006@gmail.com>
2026-03-31 10:08:16 +0800

    2222

--- public/functions/_middleware.js
+++ public/functions/_middleware.js
@@ -1,5 +1,6 @@
 export async function onRequest(context) {
-  const response = await context.next();
-  response.headers.set('My-Header', 'Test');
-  return response;
+  // const response = await context.next();
+  // response.headers.set('My-Header', 'Test');
+  // return response;
+  return new Response("Hello, world!");
 }
